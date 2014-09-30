55be63f9dc52a32ccef8604d4cd53723107c3ad3
Pedro Estrada <pedroestrada.86@gmail.com>
2014-09-30 09:49:14 -0400

    added more events to youtube_autopauser

--- youtube_autopauser.js
+++ youtube_autopauser.js
@@ -1,24 +1,47 @@
-if ($('video').length >= 1) {
+console.log("youtube_autopauser.js loaded... looking for videos");
 
-	$('video').each(function () {
-		if (!$(this).paused) {
-			console.log("youtube is playing");
-			chrome.runtime.sendMessage({message: "youtube_playing"}); // pause pandora
-		}
-	});
+var videoPlaying = null;
 
-	$('video').on('play playing', function () {
+$('video').each(function () {
+	if (!$(this).paused) {
+		videoPlaying = true;
 		console.log("youtube is playing");
 		chrome.runtime.sendMessage({message: "youtube_playing"}); // pause pandora
-	});
+	}
+});
 
-	$('video').on('pause ended', function () {
-		console.log("youtube is paused");
-		chrome.runtime.sendMessage({message: "youtube_paused"}); // play pandora
-	});
+$(document).on('click', '.html5-video-container', function () {
+	console.log('trying to start video');
+	videoPlaying = true;
+	console.log("youtube is playing");
+	chrome.runtime.sendMessage({message: "youtube_playing"}); // pause pandora
+});
 
-	$(window).on('beforeunload', function () {
-		console.log("window is closing");
+$(document).on('click', 'video', function () {
+	if ($(this).data('autoPauserEvents') == "done") {
+		// do nothing
+	}
+	else {
+		// bind events to video
+		$(this).on('play playing', function () {
+			videoPlaying = true;
+			console.log("youtube is playing");
+			chrome.runtime.sendMessage({message: "youtube_playing"}); // pause pandora
+		});
+
+		$(this).on('pause ended', function () {
+			videoPlaying = false;
+			console.log("youtube is paused");
+			chrome.runtime.sendMessage({message: "youtube_paused"}); // play pandora
+		});
+
+		$(this).data('autoPauserEvents', 'done');
+	}
+});
+
+$(window).on('beforeunload', function () {
+	console.log("window is closing");
+	if (videoPlaying != null) {
 		chrome.runtime.sendMessage({message: "youtube_paused"}); // play pandora
-	});
-}
+	}
+});
