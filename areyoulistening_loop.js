54fba6e1620fcd1ac78332c9b92fe2ff2dba0b1e
Christian Harris <christianh104@gmail.com>
2014-12-29 03:19:43 -0800

    DEBUG variable to prevent annoying logging.
    Every 10 seconds it logs to the console. Extremely annoying.

--- areyoulistening_loop.js
+++ areyoulistening_loop.js
@@ -1,9 +1,15 @@
-﻿function loop() {
+var DEBUG = false;
+
+function loop() {
     if (document.querySelectorAll("a.still_listening").length != 0){
         document.querySelectorAll("a.still_listening")[0].click();
-        console.log("Successfully clicked 'Are you still listening?' button");
+        if (DEBUG) {
+			console.log("Successfully clicked 'Are you still listening?' button");
+		}
     }
-    console.log("Music is still playing...");
+    if (DEBUG) {
+		console.log("Music is still playing...");
+	}
     setTimeout(loop, 10 * 1000); // 10 seconds
 }
 loop();
