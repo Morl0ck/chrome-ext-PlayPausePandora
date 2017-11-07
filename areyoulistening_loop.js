d9b29647a4475871dd6b7bc8540e17298ad456b1
ultipedro <32171388+ultipedro@users.noreply.github.com>
2017-11-07 14:17:28 -0500

    pandora changed the element for the are you listening button

--- areyoulistening_loop.js
+++ areyoulistening_loop.js
@@ -1,15 +1,15 @@
 var DEBUG = false;
 
 function loop() {
-    if (document.querySelectorAll("a.still_listening").length != 0){
-        document.querySelectorAll("a.still_listening")[0].click();
-        if (DEBUG) {
+	if (document.querySelectorAll("[data-qa=keep_listening_button]").length != 0){
+        	document.querySelectorAll("[data-qa=keep_listening_button]")[0].click();
+        	if (DEBUG) {
 			console.log("Successfully clicked 'Are you still listening?' button");
 		}
-    }
-    if (DEBUG) {
+	}
+	if (DEBUG) {
 		console.log("Music is still playing...");
 	}
-    setTimeout(loop, 10 * 1000); // 10 seconds
+	setTimeout(loop, 10 * 1000); // 10 seconds
 }
 loop();
